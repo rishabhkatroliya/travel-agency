0809e31b1b14b60a4042bbceefc89599d2725ba6
Swagh001 <shaileshwagh300@gmail.com>
2023-07-05 14:32:01 +0530

    Hilling � - Travel Agency app added

---
+++ hilling_travel_agency/src/AllRoutes/AllRoutes.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Routes,Route} from "react-router-dom";
+import Home from '../Component/Home';
+
+const AllRoutes = () => {
+  return (
+   <Routes>
+        <Route path='/' element={<Home/>}>Home</Route> {/* Shailesh*/}
+
+        <Route path='/ourwords' element={""}>service and product details page</Route>       {/* gaurav*/}
+        <Route path='/ourwords/:id' element={""}>Product Details page</Route>  {/* gaurav*/}
+
+        <Route path='/signup' element={""}>Signup</Route>  {/* Akash*/}
+        <Route path='/login' element={""}>Login</Route>  {/* Akash*/}
+
+        <Route path='/address' element={""}>Address</Route>  {/* Akhil*/}
+        <Route path='/payment' element={""}>Payment</Route>  {/* Akhil*/}
+
+        <Route path='/blog' element={""}>Home</Route> {/*Rishabh */}
+   </Routes> 
+  )
+}
+
+export default AllRoutes
---
+++ hilling_travel_agency/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import {Box} from "@chakra-ui/react"
+import AllRoutes from './AllRoutes/AllRoutes';
+
+function App() {
+  return (
+    <div className="App">
+        Enjoy
+        <Box>ha</Box>
+        <AllRoutes/>     
+    </div>
+    
+  );
+}
+
+export default App;
