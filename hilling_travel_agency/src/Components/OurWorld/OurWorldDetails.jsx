93f2c893586da0fdfddcd334899097e23e98e861
Gaurav <gauravhpanchal2@gmail.com>
2023-07-07 22:29:50 +0530

    OurWorld Details Page

--- hilling_travel_agency/src/Components/OurWorld/OurWorldDetails.jsx
+++ hilling_travel_agency/src/Components/OurWorld/OurWorldDetails.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { Box, Flex, HStack, Text } from "@chakra-ui/react";
+import { StarIcon } from "@chakra-ui/icons";
 
 const OurWorldDetails = () => {
   const { id } = useParams();
@@ -17,8 +19,32 @@ const OurWorldDetails = () => {
   };
   return (
     <div>
-      {" "}
-      <h1>{property.title}</h1>
+      <Box
+        p={"10px 210px"}
+        backgroundColor={"rgb(124, 141, 211)"}
+        textAlign={"left"}
+      >
+        <Text fontSize="26px" color={"white"} fontWeight={500}>
+          {property.title}
+        </Text>
+        <Flex>
+          <Box>
+            <HStack>
+              <StarIcon color={"black"} />
+              <Text fontSize="lg" color={"white"}>
+                {property.rating} ·
+              </Text>
+              <Text fontSize="lg" ml="2px" as="u" color={"white"}>
+                {property.reviewCount} reviews
+              </Text>
+              <Text color={"white"}>·</Text>
+            </HStack>
+          </Box>
+          <Box>
+            <HStack></HStack>
+          </Box>
+        </Flex>
+      </Box>
     </div>
   );
 };
