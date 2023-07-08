afe3d089af89988e0e3267e92f04e4d48dd0d807
shailesh wagh <shaileshwagh300@gmail.com>
2023-07-08 16:26:11 +0530

    ..

--- hilling_travel_agency/src/Routes/AllRoutes.js
+++ hilling_travel_agency/src/Routes/AllRoutes.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import {Routes,Route} from "react-router-dom";
 import Home from '../Components/Home/Home';
+import OurWorld from "../Components/OurWorld/OurWorld.jsx";
 
 const AllRoutes = () => {
   return (
    <Routes>
         <Route path='/' element={<Home/>}>Home</Route> {/* Shailesh*/}
 
-        <Route path='/ourwords' element={""}>service and product details page</Route>       {/* gaurav*/}
+        <Route path='/ourwords' element={<OurWorld/>}>service and product details page</Route>       {/* gaurav*/}
         <Route path='/ourwords/:id' element={""}>Product Details page</Route>  {/* gaurav*/}
 
         <Route path='/signup' element={""}>Signup</Route>  {/* Akash*/}
